3b7012f08de4254f4a0952c3ca8cbc863adab6c6
Leto <mikhaylichenko2000@bk.ru>
2022-08-10 15:00:06 +0300

    Выполнил задачу №13

---
+++ WH13/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+Console.Clear();
+
+Console.WriteLine("Введите число");
+string number = Console.ReadLine();
+
+if (number.Length <= 2) Console.WriteLine($"Третьей цифры нет");
+else Console.WriteLine($"Третья цифра числа {number} равна: {number[2]}");
